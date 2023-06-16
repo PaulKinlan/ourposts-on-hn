173f44e2129d9b9aaed3f8b6d68d01614c854f8a
Paul Kinlan <paul.kinlan@gmail.com>
2023-06-16 14:10:15 +0000

    mult

--- routes/index.tsx
+++ routes/index.tsx
@@ -8,15 +8,21 @@ const fetchHNPosts = async (url: string) => {
   return await response.json();
 }
 
-export default async function Home() {
-  const data = await fetchHNPosts("https://web.dev");
+export default async function Home(props: any) {
+  
+  const webDevData = await fetchHNPosts("https://web.dev");
+  const dcc = await fetchHNPosts("https://developer.chrome.com");
   
   return html(
     {
       lang: "en",
       title: "Chrome DevRel's posts on HN",
-      body: <div class="p-4 mx-auto max-w-screen-md"><ul>
-        {data.hits.map((post: any) => ( <li><a href={"/comments/?id=" + post.objectID}>{post.title}</a></li> ))}
+      body: <div class="p-4 mx-auto max-w-screen-md">
+        <h2>web.dev</h2><ul>
+        {webDevData.hits.map((post: any) => ( <li><a href={"/comments/?id=" + post.objectID}>{post.title}</a></li> ))}
+        </ul>
+        <h2>developer.chrome.com</h2><ul>
+        {dcc.hits.map((post: any) => ( <li><a href={"/comments/?id=" + post.objectID}>{post.title}</a></li> ))}
         </ul>
       </div>
     }
